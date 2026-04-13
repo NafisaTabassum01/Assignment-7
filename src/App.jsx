8c4ccdcbd6a4a0b168875fad6c9fd590eeb87051
NafisaTabassum01 <tabassumnafisa28.01@gmail.com>
2026-04-14 00:20:38 +0600

    friends added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { Suspense } from 'react'
 import './App.css'
 import Banner from './Banner/Banner'
 import Friends from './Friends/Friends'
 import Navbar from './Navbar/Navbar'
 
+
+const fetchFriends = async ()=> {
+  const res = await fetch("/data.json")
+  return res.json();
+}
+
 function App() {
 
+  const friendsPromise = fetchFriends();
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
-      <Friends></Friends>
+    <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>     
+     <Friends friendsPromise={friendsPromise}></Friends>
+    </Suspense>
       </>
   )
 }
--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -7,24 +7,24 @@ const Banner = () => {
                 <h1 className='font-bold text-[48px]'>Friends to keep close in your life</h1>
                 <p className='text-[16px] text-gray-400 mt-4'>Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />
 relationships that matter most.</p>
-<button className='btn bg-[#244D3F] text-white my-6'>+ Add a Friend</button>
+           <button className='btn bg-[#244D3F] text-white my-6'>+ Add a Friend</button>
             </div>
-            <div className='flex justify-center gap-5 pb-8 pt-2 w-full'>
-                <div className='grid grid-cols-1 bg-white text-center px-14 py-7'>
+            <div className='md:flex justify-center gap-5 pb-8 pt-2 w-full'>
+                <div className='md:grid grid-cols-1 bg-white text-center px-14 py-7 mb-2'>
                     <p className='font-semibold text-[32px] text-[#244D3F]'>10</p>
                     <p className='text-[18px] text-[#64748B]'>Total Friends</p>
                 </div>
-                <div className='grid grid-cols-1 bg-white text-center px-14 py-7'>
-                    <p className='font-semibold text-[32px] text-[#244D3F]'>10</p>
-                    <p className='text-[18px] text-[#64748B]'>Total Friends</p>
+                <div className='grid grid-cols-1 bg-white text-center px-14 py-7 mb-2'>
+                    <p className='font-semibold text-[32px] text-[#244D3F]'>3</p>
+                    <p className='text-[18px] text-[#64748B]'>On Track</p>
                 </div>
-                <div className='grid grid-cols-1 bg-white text-center px-14 py-7'>
-                    <p className='font-semibold text-[32px] text-[#244D3F]'>10</p>
-                    <p className='text-[18px] text-[#64748B]'>Total Friends</p>
+                <div className='grid grid-cols-1 bg-white text-center px-14 py-7 mb-2'>
+                    <p className='font-semibold text-[32px] text-[#244D3F]'>6</p>
+                    <p className='text-[18px] text-[#64748B]'>Need Attention</p>
                 </div>
-                <div className='grid grid-cols-1 bg-white text-center px-14 py-7'>
-                    <p className='font-semibold text-[32px] text-[#244D3F]'>10</p>
-                    <p className='text-[18px] text-[#64748B]'>Total Friends</p>
+                <div className='grid grid-cols-1 bg-white text-center px-14 py-7 mb-2'>
+                    <p className='font-semibold text-[32px] text-[#244D3F]'>12</p>
+                    <p className='text-[18px] text-[#64748B]'>Interactions This Month</p>
                 </div>
             </div>
             <hr className='border border-gray-200'/>
